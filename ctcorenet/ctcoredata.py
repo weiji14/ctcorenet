5a59d8f3c7f2d7b9a7bae50f4362b95994c34972
Wei Ji <23487320+weiji14@users.noreply.github.com>
2021-09-27 23:36:30 +1300

    :hankey: Implement predict mode for CTCoreDataset
    Part 1 of coding up prediction/inference logic for the CTCoreDataset, i.e. loading only the CT core image data with no mask labels. Will be used when calling `trainer.predict` later. Image has to be resized to (4096, 512) so that Unet model works (might need to handle 90deg rotated images too). Also storing the original shape in a list so that we can resize them back later.

--- ctcorenet/ctcoredata.py
+++ ctcorenet/ctcoredata.py
@@ -11,6 +11,7 @@ import typing
 import pytorch_lightning as pl
 import torch
 import torchvision
+from torch.nn import functional as F
 
 
 class CTCoreDataset(torch.utils.data.Dataset):
@@ -29,35 +30,59 @@ class CTCoreDataset(torch.utils.data.Dataset):
         'data/train/'.
     """
 
-    def __init__(self, images_dir: str = "data/train/"):
-        self.image_paths: typing.List[str] = sorted(
-            glob.glob(os.path.join(images_dir, "**", "img.png"))
+    def __init__(self, images_dir: str = "data/train/", predict: bool = False):
+        self.images_dir: str = images_dir
+        self.predict: bool = predict
+
+        pathname: str = (
+            os.path.join(self.images_dir, "*.jpg")
+            if self.predict
+            else os.path.join(self.images_dir, "**", "img.png")
         )
+        self.image_paths: typing.List[str] = sorted(glob.glob(pathname=pathname))
+
         # List of raw JPG images of CT Core and classified pixel labels
         self.images_and_labels: typing.List[torch.Tensor] = []
+        # List of raw image shapes (channel, height, width) before resizing
+        self.original_shapes: typing.List[tuple] = []
 
         # Generate training dataset with augmentation
         for img_path in self.image_paths:
-            # Load image and label from file
+            # Load image from file
             image: torch.Tensor = torchvision.io.read_image(
                 path=img_path, mode=torchvision.io.ImageReadMode.GRAY
             )
-            label: torch.Tensor = torchvision.io.read_image(
-                path=os.path.join(os.path.dirname(img_path), "label.png")
-            )
-            assert image.shape == label.shape
-            image_and_label = torch.cat(tensors=[image, label])
-            # print(image_and_label.shape)
 
-            # Ensure standard tensor size
-            five_crop_transform = torchvision.transforms.FiveCrop(size=(256, 256))
-            self.images_and_labels.extend(five_crop_transform(image_and_label))
-            random_crop_transform = torchvision.transforms.RandomCrop(
-                size=(256, 256), padding_mode="symmetric"
-            )
-            self.images_and_labels.extend(
-                [random_crop_transform(image_and_label) for _ in range(123)]
-            )
+            # Prediction mode - Load only CT Core images
+            if self.predict:
+                # Resize image to standard shape so that they can be stacked
+                original_shape: tuple = image.shape
+                self.original_shapes.append(tuple(original_shape))
+
+                tensor: torch.Tensor = torch.stack(tensors=[image]).float()
+                _image: torch.Tensor = F.interpolate(
+                    input=tensor, size=(4096, 512), mode="bilinear", align_corners=True
+                )
+                self.images_and_labels.append(_image[0])
+
+            # Training mode - Load CT Core images and Groundtruth labels
+            else:
+                label: torch.Tensor = torchvision.io.read_image(
+                    path=os.path.join(os.path.dirname(img_path), "label.png")
+                )
+                assert image.shape == label.shape
+                image_and_label = torch.cat(tensors=[image, label])
+                # print(image_and_label.shape)
+
+                # Ensure standard tensor size
+                five_crop_transform = torchvision.transforms.FiveCrop(size=(256, 256))
+                self.images_and_labels.extend(five_crop_transform(image_and_label))
+                random_crop_transform = torchvision.transforms.RandomCrop(
+                    size=(256, 256), padding_mode="symmetric"
+                )
+                self.images_and_labels.extend(
+                    [random_crop_transform(image_and_label) for _ in range(123)]
+                )
 
         self.ids = [i for i in range(len(self.images_and_labels))]
 
@@ -87,15 +112,21 @@ class CTCoreDataModule(pl.LightningDataModule):
     images_dir : str
         Filepath to the folder containing the CT Core images. Default is
         'data/train/'.
+
+    predict : bool
+        Whether the data module is being used for prediction on new images, in
+        which case no groundtruth labels are to be loaded. Default is False,
+        i.e. groundtruth labels will be loaded from a label.png file.
     """
 
-    def __init__(self, images_dir: str = "data/train/"):
+    def __init__(self, images_dir: str = "data/train/", predict: bool = False):
         """
         Define image data storage location and data containers for storing
         preprocessed images and labels.
         """
         super().__init__()
         self.images_dir: str = images_dir
+        self.predict: bool = predict
 
     def prepare_data(self):
         """
@@ -104,7 +135,7 @@ class CTCoreDataModule(pl.LightningDataModule):
         """
         # Create a proper Pytorch Dataset from tuple of (images, targets)
         self.dataset: torch.utils.data.Dataset = CTCoreDataset(
-            images_dir=self.images_dir
+            images_dir=self.images_dir, predict=self.predict
         )
 
     def setup(self, stage: typing.Optional[str] = None) -> torch.utils.data.Dataset:
@@ -120,3 +151,10 @@ class CTCoreDataModule(pl.LightningDataModule):
         Set the training batch size here too.
         """
         return torch.utils.data.DataLoader(dataset=self.dataset, batch_size=32)
+
+    def predict_dataloader(self) -> torch.utils.data.DataLoader:
+        """
+        Loads the data used in the prediction loop.
+        Set the prediction batch size here too.
+        """
+        return torch.utils.data.DataLoader(dataset=self.dataset, batch_size=8)
